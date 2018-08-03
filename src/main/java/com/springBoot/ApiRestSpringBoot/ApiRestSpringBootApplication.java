17134847260b9627f8661c0afc8b8b524a907cea
unknown <csrtr94@gmail.com>
2018-08-03 04:09:19 -0400

    first commit

---
+++ src/main/java/com/springBoot/ApiRestSpringBoot/ApiRestSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.springBoot.ApiRestSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringBootApplication.class, args);
+	}
+}
